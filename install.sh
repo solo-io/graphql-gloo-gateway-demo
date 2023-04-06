6d22b25d11460711fb71b1e2683b0dd9fb06e073
asayah <adam@solo.io>
2023-04-06 12:07:32 -0700

    adding gw features lab

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash 
 export GLOO_VERSION=2.3.0-beta4
-#license=eyJhZGRPbnMiOltdLCJleHAiOjE2ODMxNTU0NTMsImlhdCI6MTY4MDU2MzQ1MywiayI6IjA1ZnpTQSIsImx0IjoiZW50IiwicHJvZHVjdCI6Imdsb28tZ2F0ZXdheSJ9.HE68j9NgTtPJlQFzgMAq_rl4b_xCHoZ7tozeBZ7ZP9s
-license=eyJhZGRPbnMiOlt7IkFkZG9uIjowLCJFeHBpcmVzQXQiOjE2ODg0Mjg5MDYsIkxpY2Vuc2VUeXBlIjoiZW50In1dLCJleHAiOjE2ODg0Mjg5MDYsImlhdCI6MTY2MjUwODkwNiwiayI6InJ1ZDExZyIsImx0IjoiZW50IiwicHJvZHVjdCI6Imdsb28tdHJpYWwifQ.by7jozgqnuvVDigCfnO5cZMsY3pklnIpXIt8wDPejgM
+license=$1
 
 helm repo add gloo-platform https://storage.googleapis.com/gloo-platform/helm-charts
 helm repo update
@@ -16,6 +15,6 @@ helm install gloo-platform gloo-platform/gloo-platform \
    --version $GLOO_VERSION \
    --values ./install/gloo-gateway.yaml \
    --set licensing.glooGatewayLicenseKey=$license
-   #--set licensing.glooTrialLicenseKey=$license
    
-   #glooGatewayLicenseKey
+# Creating defaults     
+kubectl apply -f ./install/defaults
